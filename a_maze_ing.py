57fb174fe114e217d93f0319d7b6a9784d3799de
Aymane El Halkouj <aymel-ha@e2r6p12.1337.ma>
2026-02-17 01:10:52 +0000

    amazing

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -5,7 +5,7 @@ from typing import Any, List, Dict, Union, Optional
 
 class A_Maze_Ing:
     def __init__(self):
-        self.tokens = self.Maze_Config_Analyzer.parsing_config_data()
+        self.tokens = self.Maze_config_analyzer.parsing_config_data()
         self.width = self.tokens["WIDTH"]
         self.height = self.tokens["HEIGHT"]
         self.entry = self.tokens["ENTRY"]
@@ -14,9 +14,9 @@ class A_Maze_Ing:
         self.seed = self.tokens["SEED"]
         self.perfect = self.tokens["PERFECT"]
         self.flawed = self.tokens["FLAWED"]
-        self.maze = self.Recursive_Backtracker(self)
+        self.maze = self.MazeGenerationParts(self)
 
-    class Maze_Config_Analyzer:
+    class Maze_config_analyzer:
         @staticmethod
         def parsing_config_data() -> dict[str, Union[str, int, None]]:
             try:
@@ -70,8 +70,11 @@ class A_Maze_Ing:
                             tokens[key] = True if value == "True" else False
 
                         case "EXIT" | "ENTRY":
-                            x, y = map(int, value.split(","))
-                            tokens[key] = {"x": x, "y": y}
+                            try:
+                                x, y = map(int, value.split(","))
+                                tokens[key] = {"x": x, "y": y}
+                            except ValueError:
+                                raise ValueError(f"Invalid/Missing {key} value")
 
                         case "FLAWED" | "SEED":
                             try:
@@ -106,34 +109,12 @@ class A_Maze_Ing:
                 print(f"Config Error: {e}")
                 sys.exit(1)
 
-    class Recursive_Backtracker:
+    class MazeGenerationParts:
         def __init__(self, maze):
-            self.maze = maze 
-        def pattern_42(self):
-            return {(0,0), (2,0), (0,1), (2,1), (0,2), (1,2), (2,2), (2,3), (2,4), (4,0), (5,0), (6,0), (6,1), (4,2), (5,2), (6,2), (4,3), (4,4), (5,4), (6,4)}
-        def shape_patter_42(self, maze):
-            pattern = self.pattern_42()
-            pattern_height = max(y for _, y in pattern) + 1
-            pattern_width = max(x for x, _ in pattern) + 1
-            half_grid_h = (maze.shape[0] - 1) // 2
-            half_grid_w = (maze.shape[1] - 1) // 2
-            
-            entry_tuple = (self.maze.entry["x"], self.maze.entry["y"])
-            exit_tuple = (self.maze.exit["x"], self.maze.exit["y"])
-            for y in range (1, half_grid_h - pattern_height):
-                for x in range (1, half_grid_w - pattern_width):
-                    possible_cells = {(x + pattern_x, y + pattern_y) for pattern_x, pattern_y in pattern}
-                    if entry_tuple not in possible_cells and exit_tuple not in possible_cells:
-                        return x, y
-            raise ValueError(f"Cannot shape the pattern, Maze is too small ({self.maze.height}, {self.maze.width})")
-            
-            
-        def grid_creator(self, height: int, width: int) -> n.ndarray:
-            maze = n.full((height, width), 0xF,dtype=n.uint8)
-            return maze
-        
+            self.maze = maze
+
         @staticmethod
-        def iterative_recursion_backtracker( maze, height, width, enx, eny, compass):
+        def iterative_backtracker(maze, height, width, enx, eny, compass):
             random.shuffle(compass)
             stack_simulation = [(enx, eny, compass.copy())]
             visited_cells = {(enx, eny)}
@@ -180,27 +161,10 @@ class A_Maze_Ing:
                         break
                 if not moved:
                     stack_simulation.pop()
-            
-            return maze
-        def maze_entry(self) -> n.ndarray:
-            maze = self.grid_creator(self.maze.height, self.maze.width)
-            compass = ["North", "South", "West", "East"]
-            maze = self.iterative_recursion_backtracker(maze, self.maze.height, self.maze.width, self.maze.entry["x"], self.maze.entry["y"], compass)
-            return maze
-                    
-        def imperfect_maze(self, maze, height, width, flawed):
-            removeable_walls = []
-            for y in range(1, height - 1):
-                for x in range(1, width - 1):
-                    if(maze[y,x] == 1):
-                        if (maze[y+1, x] == 0 and maze[y-1, x] == 0) or (maze[y, x+1] == 0 and maze[y, x-1] == 0):
-                            removeable_walls.append((y,x))
-            random.shuffle(removeable_walls)
-            for y, x in removeable_walls[:flawed]:
-                maze[y, x] = 0
-            return maze
 
-        def BFS(self, maze, enx: int, eny: int, exx: int, exy: int) -> str:
+            return maze
+        @staticmethod
+        def bfs(maze, enx: int, eny: int, exx: int, exy: int) -> str:
             directions = {0: (0, -1, 'N'), 1: (1, 0, 'E'), 2: (0, 1, 'S'), 3: (-1, 0, 'W')} 
             queue = [(enx, eny, "")]
             visited = {(enx, eny)}
@@ -213,22 +177,20 @@ class A_Maze_Ing:
                 
                 for direction, (dx, dy, compass) in directions.items():
 
-                    if not bool(self.maze[y, x] & (0x1 << direction)):
+                    if not bool(maze[y, x] & (0x1 << direction)):
                         nx, ny = x + dx, y + dy
                         
-                        if (0 < nx < width and 
-                            0 < ny < height and
+                        if (0 <= nx < width and 
+                            0 <= ny < height and
                             (nx, ny) not in visited):
 
                             visited.add((nx, ny))
                             queue.append((nx, ny, path + compass))
-            
-            return ""
-                    
 
-    
+            return "No path found"
+        
     @staticmethod
-    def maze_hexadecimal(maze, output_file, height, width, entry_p, exit_p):
+    def maze_hexadecimal(maze, output_file, height, width, entry_p, exit_p, path):
         with open(output_file, "w") as f:
             for y in range(height):  
                 row_cells = []
@@ -240,27 +202,10 @@ class A_Maze_Ing:
             exx, exy = exit_p["x"], exit_p["y"]
             print(f"\n{enx},{eny}", file=f, end="\n")
             print(f"{exx},{exy}", file=f, end="\n")
-            
-    def combine(self):
-        if self.seed is not None:
-            random.seed(self.seed)
-        
-        generated_maze = self.maze.maze_entry()
-        
-        if not self.perfect and self.flawed is not None and self.flawed > 0:
-            generated_maze = self.maze.imperfect_maze(generated_maze, self.height, self.width, self.flawed)
-                
-        if self.output_file:
-            self.maze_hexadecimal(generated_maze, self.output_file, self.height, self.width, self.entry, self.exit)
-            print(f"\nMaze saved to {self.output_file}")
-        finder = self.BFSPathfinder(generated_maze)
-        path = finder.find_path(0, 0, 19, 14)
-        print(f"Path: {path}")
-        print(f"Length: {len(path)} steps")
-        
+            print(f"{path}", file=f, end="\n")
 
-        
-    def print_arguments(self):
+
+    def start(self):
         print(f"Height: {self.height}")
         print(f"Width: {self.width}")
         print(f"Entry Point: {self.entry}")
@@ -269,8 +214,16 @@ class A_Maze_Ing:
         print(f"Seed: {self.seed}")
         print(f"Perfect: {self.perfect}")
         print(f"Flawed: {self.flawed}")
+        if self.seed:
+            random.seed(self.seed)
+        compass = ["North", "East", "South", "West"]
+        maze = n.full((self.height, self.width), 0xF,dtype=n.uint8)
+        generated_maze = self.MazeGenerationParts.iterative_backtracker(maze, self.height, self.width, self.entry['x'], self.entry['y'], compass)
+        path = self.MazeGenerationParts.bfs(generated_maze, self.entry['x'], self.entry['y'], self.exit['x'], self.exit['y'])
+        print(f"{path}")
+        self.maze_hexadecimal(generated_maze, self.output_file, self.height, self.width, self.entry, self.exit, path)
         
             
 if __name__ == "__main__":
     maze = A_Maze_Ing()
-    maze.combine()
+    maze.start()
